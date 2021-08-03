7a139982e7ca488681cc6e6f415e9d313461a977
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2021-08-03 11:12:22 +0200

    0.11.1

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ if ('serviceWorker' in navigator) {
  * Make version number available for
  */
 
-window.salemAppVersion = "0.11.0";
+window.salemAppVersion = "0.11.1";
 
 /** **********************************************************************
  * Run the game. Specify the game's DOM node.
