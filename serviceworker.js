93f1f7305c8659a14d32aa08fcc8a3fda4d9826f
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2021-08-16 20:35:17 +0200

    0.18.3

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@
  * Serviceworker for Salem 1692 Moderator
  */
 
-const version = "0.18.2";
+const version = "0.18.3";
 const cacheName = 'salem-1692-v' + version;
 //const rootDir = '.';
 const rootDir = '/salem-1692/dist';
