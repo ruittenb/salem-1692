4da7a350df90da4d8f5ed1eb0bdee2232d5f6571
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2021-12-23 10:59:16 +0100

    Removed unused code

--- src/app.js
+++ src/app.js
@@ -7,33 +7,10 @@
 // Main Salem Moderator code
 import * as Main from './Main.bs';
 
-// npm modules
 import QRCode from 'davidshimjs-qrcodejs';
 import qrCodeParser from 'qrcode-parser';
 
-/** **********************************************************************
- * Register serviceworker if supported
- */
-
-//const debug = navigator.userAgent.includes('Salem/1692') ? '?debug=1' : '';
-//
-//if ('serviceWorker' in navigator) {
-//    window.addEventListener('load', () => {
-//        navigator.serviceWorker
-//            .register('serviceworker.js' + debug)
-//            .then((registration) => {
-//                console.log('ServiceWorker registered with scope', registration.scope);
-//            })
-//            .catch((err) => {
-//                console.log('ServiceWorker registration failed:', err);
-//            });
-//    });
-//}
-
-/** **********************************************************************
- * Make some variables available for Rescript
- */
-
+// Make some variables available for Rescript
 window.salemAppVersion = "0.43.4";
 
 // QR code generator
